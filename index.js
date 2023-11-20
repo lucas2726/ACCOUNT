fc29d5c5521f1cbf4ba2778c7ab7b1d173fa6e02
LucasBueno2726 <140419660+lucas2726@users.noreply.github.com>
2023-11-20 18:01:53 -0400

    ee

---
+++ index.js
@@ -0,0 +1,26 @@
+//modulos externos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+//modulos internos
+const fs = require("fs")
+
+operation()
+
+function operation() {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: [
+                'Criar Conta','Consultar Saldo','Depositar','Sacar','Sair'],
+        },
+    ]).then(answer => {
+        const action = answer['action']
+        console.log(action)
+    }).catch((err) => console.log(err))
+}
+
+console.log("Iniciamos o Accounts")
+                
